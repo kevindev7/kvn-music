75d0826993a3126608735d13cae6d4018c8a78a5
Just a human <79577297+Kevin5057@users.noreply.github.com>
2021-12-07 22:03:21 +0700

    Updating the Embed

--- player/searchResults.js
+++ player/searchResults.js
@@ -1,9 +1,9 @@
 module.exports = (client, message, query, tracks) => {
     message.channel.send({
         embed: {
-            color: 'BLUE',
-            author: { name: `Here are your search results for ${query}` },
-            footer: { text: 'Npg op' },
+            color: '2f3136',
+            author: { name: `Here are your search results for "${query}"`, iconURL: client.user.displayAvatarURL() },
+            footer: { text: '©2021 K V N Development' },
             timestamp: new Date(),
             description: `${tracks.map((t, i) => `**${i + 1}** - ${t.title}`).join('\n')}`,
         },
